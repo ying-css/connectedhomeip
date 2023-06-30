3b78a8dbd9b9734853d9b179d4b04900aa8d0104
Ye Ying (IFAP CSS SMD AP TM SAE ITS) <Ying.Ye@infineon.com>
2023-06-30 10:16:48 +0800

    1)Updated README_TrustM.md

--- examples/lock-app/infineon/psoc6/include/CHIPProjectConfig.h
+++ examples/lock-app/infineon/psoc6/include/CHIPProjectConfig.h
@@ -34,8 +34,8 @@
 #endif
 #define CHIP_DEVICE_CONFIG_USE_TEST_SETUP_DISCRIMINATOR 0xF00
 
-/* The ProductID attribute of the Basic cluster. */
-#define CHIP_DEVICE_CONFIG_DEVICE_PRODUCT_ID 0x8001
+/* The ProductID attribute of the example lock-app */
+#define CHIP_DEVICE_CONFIG_DEVICE_PRODUCT_ID 0x8006
 
 /* The ProductName attribute of the Basic cluster. */
 #define CHIP_DEVICE_CONFIG_DEVICE_PRODUCT_NAME "P6-CYW43012 Lock"
